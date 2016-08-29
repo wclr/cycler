1741fd1cf94e137843c87cb0f7a23cb31d721470
whitecolor <alex.oshchepkov@gmail.com>
2016-08-30 04:50:36 +0500

    initial common files

---
+++ make-tsconfig.js
@@ -0,0 +1,10 @@
+var dir = process.argv[2]
+var config = require(__dirname + '/tsconfig.json')
+config.include = [
+  "../typings",
+  "src"
+]
+require('fs').writeFileSync(
+  __dirname + '/' + dir + '/tsconfig.json',
+  JSON.stringify(config, null, 2)
+) 
