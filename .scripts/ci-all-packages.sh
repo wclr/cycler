476cc7146bc37d136f0376890e421e7f9ab844b2
whitecolor <alex.oshchepkov@gmail.com>
2016-12-11 22:21:19 +0500

    fix publish cycler-built script

--- .scripts/ci-all-packages.sh
+++ .scripts/ci-all-packages.sh
@@ -2,18 +2,18 @@
 
 # This script is for executing command 
 # for all package directories.
-BUILD_SOURCE_CMD=../node_modules/.bin/tsc --outDir .
-TEST_SOURCE_CMD=node -r source-map-support/register test/test.js
-sh ./.scripts/for-all-packages.sh yarn && $BUILD_SOURCE_CMD && $TEST_SOURCE_CMD
+BUILD_SOURCE_CMD="../node_modules/.bin/tsc --outDir ."
+TEST_SOURCE_CMD="node -r source-map-support/register test/test.js"
+sh ./.scripts/for-all-packages.sh "yarn && $BUILD_SOURCE_CMD && $TEST_SOURCE_CMD"
 
 SHA=$(git rev-parse HEAD)
 
-# url where built packages are located
+# Url where built packages are located
 BUILT_URL=https://github.com/cycler-built
 
 sh ./.scripts/for-all-packages.sh " \
   rm -rf .git &&\
-  git init &&\
+  git init &&\  
   git remote add built $BUILT_URL/\$d &&\
   git remote show built &&\
   echo -e node_modules'\\n'yarn-error.log > .gitignore &&\
@@ -23,3 +23,5 @@ sh ./.scripts/for-all-packages.sh " \
   rm -rf .git .gitignore
   "\
   /
+
+#sh ./.scripts/for-all-packages.sh "echo dir:\$d"
--- .scripts/for-all-packages.sh
+++ .scripts/for-all-packages.sh
@@ -2,17 +2,14 @@
 
 # This script is for executing command 
 # for all package directories.
-$exitstatus=0
+exitstatus=0
 
 while read d; do
   echo "> $d";
   echo "> $@";
   echo "";
   cd $d;
-  #sh -c ls && ls
-  sh -c "${@/\$d/$d}"
-  #${@/\$d/$d} || exitstatus=$?;
-  #bash -c ls || exitstatus=$?;
+  sh -c "${@/\$d/$d}" || exitstatus=$?;
   cd ..;
   if [[ $exitstatus -ne 0 ]]; then
     break;
