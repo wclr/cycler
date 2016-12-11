5bfe52f241fe7e6cd1a1f2e9348fa594083a2a9a
whitecolor <alex.oshchepkov@gmail.com>
2016-12-11 22:51:29 +0500

    fix ci-all-packages add git identity

--- .scripts/ci-all-packages.sh
+++ .scripts/ci-all-packages.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# This script is for executing command 
-# for all package directories.
+
+# Install packages deps, build and run tests
+
 BUILD_SOURCE_CMD="../node_modules/.bin/tsc --outDir ."
 TEST_SOURCE_CMD="node -r source-map-support/register test/test.js"
 
+# Make script it executable: on travis CI need to run it with bash (sh doesn't work), 
+# on windows `bash script.sh` fails
 chmod +x ./.scripts/for-all-packages.sh
 
 ./.scripts/for-all-packages.sh "yarn && $BUILD_SOURCE_CMD && $TEST_SOURCE_CMD"
 
+# "Publish" built packages to github.com/cycler-built/<package>
+
+# Get SHA of current cycler commit
 SHA=$(git rev-parse HEAD)
+
+# Set git identity
+git config --global user.email "alexosh@me.com"
+git config --global user.name "whitecolor"
 
 # Url where built packages are located
 BUILT_URL=https://github.com/cycler-built
 
+# Create temorary git repo inside the package 
+# and replace cycler-built/<package> repo with new source
 ./.scripts/for-all-packages.sh " \
   rm -rf .git &&\
   git init &&\  
@@ -26,5 +38,3 @@ BUILT_URL=https://github.com/cycler-built
   rm -rf .git .gitignore
   "\
   /
-
-#sh ./.scripts/for-all-packages.sh "echo dir:\$d"
