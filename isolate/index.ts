2d5d0a5d10ae25882ab21b0073bc284e0c9523bb
A O <alex.oshchepkov@gmail.com>
2020-08-28 21:39:16 +0500

    remove @cycler/isolate

--- isolate/index.ts
+++
@@ -1,9 +0,0 @@
-import cycleIsolate from '@cycle/isolate'
-
-export interface Isolate {
-  // <Sources, Sinks>(component: (sources: Sources) => Sinks, scope?: any):
-  //   (sources: Sources) => Sinks
-  <IsolatedComponent extends Function>(component: IsolatedComponent, scope?: any): IsolatedComponent
-}
-
-export default cycleIsolate as Isolate
